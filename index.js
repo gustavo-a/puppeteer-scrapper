4de86dc32289377f3372515e7bff697f7d1c2ddb
Gustavo de Andrade <gustavo_641@live.com>
2018-12-28 01:40:35 -0200

    Code optimization

--- index.js
+++ index.js
@@ -1,54 +1,58 @@
-const puppeteer = require('puppeteer');
-const fs = require('fs-extra');
-const cookies = require('./cookies'); //log into facebook and paste all your cookies here
+const puppeteer = require("puppeteer");
+const fs = require("fs-extra");
+const cookies = require("./cookies"); //log into facebook and paste all your cookies here
 
-(async function main(){
+(async function main() {
 
-  try{
-		const browser = await puppeteer.launch({ headless: true });
-		const page = await browser.newPage();
+    try {
+        const browser = await puppeteer.launch({
+            headless: true
+        });
+        const page = await browser.newPage();
 
-		//use your actual User Agent so Facebook won't trigger any authentication issues
-		page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 OPR/57.0.3098.106');
+        //use your actual User Agent so Facebook won't trigger any authentication issues
+        page.setUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 OPR/57.0.3098.106");
 
-		const url = "https://www.facebook.com/search/364721450401244/likers/females/110346955653479/residents-near/present/intersect/";
-		const nameSelector = "._32mo" //selector that contains our data
+        await page.setCookie(...cookies);
+
+        const url = "https://www.facebook.com/search/364721450401244/likers/females/110346955653479/residents-near/present/intersect/";
+        const nameSelector = "._32mo" //selector that contains our data
+        let jsonOutput = new Array;
 
 
-		await page.setCookie(...cookies);
-		await page.goto(url);
-		await page.waitForSelector(nameSelector);
+        await page.goto(url);
+        await page.waitForSelector(nameSelector);
 
-		await page.evaluate( async () =>{
-			await new Promise((resolve, reject) =>{
-				let interval = setInterval(() => {
-					window.scrollBy(0, 200000)
-					if (document.querySelector("#browse_end_of_results_footer")){ //this is the element that shows when the search results are over
-						clearInterval(interval)
-						resolve()
-					}
-				}, 500)
-			})
-		})
+        await page.evaluate(async () => {
+            await new Promise((resolve, reject) => {
+                let interval = setInterval(() => {
+                    window.scrollBy(0, 200000)
+                    if (document.querySelector("#browse_end_of_results_footer")) { //this is the element that shows when the search results are over
+                        clearInterval(interval)
+                        resolve()
+                    }
+                }, 500)
+            })
+        })
 
-		const names = await page.$$(nameSelector);
+        const names = await page.$$(nameSelector);
 
-		await fs.writeFile('out.csv', 'nome,link\n')
+        for (let i = 0; i < names.length; i++) {
 
-		for (let i = 0; i < names.length; i++){
+            const name = names[i];
+            const url = await page.evaluate(name => name.href, name);
 
-			const name = names[i];
-			const url = await page.evaluate(name => name.href, name);
-			const text = await page.evaluate(name => name.innerText, name);
+            jsonOutput[i] = {
+                "link": url
+            }
+        }
+        await fs.writeFile("./output/linksList.json", JSON.stringify(jsonOutput))
 
-			await fs.appendFile('out.csv', `${text},${url}\n`)
-		}
+        console.log("done");
 
-		console.log('done');
+        await browser.close();
 
-		await browser.close();
-
-	} catch(e){
-    console.log('error', e)
-  }
+    } catch (err) {
+        console.error(err)
+    }
 })();
